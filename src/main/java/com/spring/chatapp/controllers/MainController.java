2b16cd5c0c2e87b645055a2051358df155e4ef1c
victorpena13 <victor.pena1392@gmail.com>
2020-05-06 18:05:36 -0500

    added main controller

---
+++ src/main/java/com/spring/chatapp/controllers/MainController.java
@@ -0,0 +1,49 @@
+package com.spring.chatapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index(HttpServletRequest request, Model model) {
+        String username = (String) request.getSession().getAttribute("username");
+
+        if(username == null || username.isEmpty()) {
+            return "redirect:/login";
+        }
+        model.addAttribute("username", username);
+
+        return "chat";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String doLogin(HttpServletRequest request, @RequestParam(defaultValue = "") String username) {
+        username = username.trim();
+
+        if(username.isEmpty()) {
+            return "login";
+        }
+        request.getSession().setAttribute("username", username);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession(true).invalidate();
+
+        return "redirect:/login";
+    }
+}
