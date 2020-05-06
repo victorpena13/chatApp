d5d1eeabe07065ff54e0b928151c9f87882a7fdb
victorpena13 <victor.pena1392@gmail.com>
2020-05-06 18:10:48 -0500

    added WebSocketController

---
+++ src/main/java/com/spring/chatapp/controllers/WebSocketController.java
@@ -0,0 +1,25 @@
+package com.spring.chatapp.controllers;
+
+import com.spring.chatapp.models.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
